48aeeed8768280806c527d817bbc39d76bcd5d33
root <root@localhost.localdomain>
2016-05-19 01:26:05 +0430

    Start

---
+++ Main.js
@@ -0,0 +1,82 @@
+/**
+ * Created by Mozamel Saber on 7/23/2015.
+ */
+
+$(document).ready(function(){
+    setTimeout(function(){
+        $('body').addClass('loaded');
+
+    },3000);
+});
+$(document).ready(function(){
+    setTimeout(function(){
+        $('#content').prepend('<p>Hello!<br>سلام!<br>Salut!<br>Hallo!<br>你好!<br>Bonjour<br>Merhaba!<br>Namaste<br>Zdravstvujtye!<br>Hej!<br>adaab! ');
+
+    },5000);});/*
+$(document).ready(function(){
+    setTimeout(function(){
+        $('#content').prepend('<p>Urghh! I like my website to be simple');
+
+    },7000);
+});
+setTimeout(function() {
+  $("#content p").remove();
+}, 11000);
+	
+});
+
+$(document).ready(function(){
+    setTimeout(function(){
+        $('#content').prepend("<p>Sooo I'm going to remove all of this,");
+								  
+    },12000);
+	setTimeout(function() {
+  $("#content p").remove();
+}, 18000);
+
+
+});
+$(document).ready(function(){
+    setTimeout(function(){
+        $('#content').prepend("<p>UserID: Simplicity &nbsp;&nbsp;&nbsp;&nbsp; Password: *************");
+    },19000);
+	setTimeout(function() {
+  $("#content p").remove();
+}, 26000);
+
+});
+$(document).ready(function(){
+    setTimeout(function(){
+        $('#content').prepend("<p>1010100101010000000011111111100000111111010");
+		$('#content').prepend("<p>1010100101010000000011111111100000111111010");
+		$('#content').prepend("<p>1010100101010000000011111111100000111111010");
+		$('#content').prepend("<p>1010100101010000000011111111100000111111010");
+		$('#content').prepend("<p>1010100101010000000011111111100000111111010");
+		$('#content').prepend("<p>1010100101010000000011111111100000111111010");
+		$('#content').prepend("<p>1010100101010000000011111111100000111111010");
+		$('#content').prepend("<p>1010100101010000000011111111100000111111010");
+    },27000);
+	setTimeout(function() {
+  $("#content p").remove();
+}, 35000);
+
+});
+$(document).ready(function(){
+    setTimeout(function(){
+        $('#content').prepend("<p>PERMISSION FOR ACCESS...................................");
+    },35000);
+    setTimeout(function(){
+        $('#content').prepend("<p>ACCESS GRANTED");
+    },40000);
+	setTimeout(function() {
+  $("#content p").remove();
+}, 42000);
+});
+
+$(document).ready(function(){
+    setTimeout(function(){
+        $('nav').prepend(".navbar navbar-inverse navbar-fixed-top").remove();
+		
+    },43000);
+ 
+}); */
